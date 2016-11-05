e148d6b20edda0d476d1de3235826c73e54189e9
Glenn Espinosa <glennpeter.espinosa@gmail.com>
2016-11-05 11:20:40 -0400

    Add loading of sampleFish and displaying via Fish component

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -4,11 +4,11 @@ class AddFishForm extends React.Component {
   createFish(event) {
     event.preventDefault();
     const fish = {
-      name: this.name.value,
-      price: this.price.value,
+      name:   this.name.value,
+      price:  this.price.value,
       status: this.status.value,
-      desc: this.desc.value,
-      image: this.image.value,
+      desc:   this.desc.value,
+      image:  this.image.value,
     }
     this.props.addFish(fish);
     this.fishForm.reset()
--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,10 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './Fish';
+
+// Misc
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
   constructor() {
@@ -11,6 +15,7 @@ class App extends React.Component {
 
     // functions
     this.addFish = this.addFish.bind(this)
+    this.loadSamples = this.loadSamples.bind(this)
 
     // state
     this.state = {
@@ -20,23 +25,40 @@ class App extends React.Component {
   }
 
   addFish(fish) {
-    // update our state
     const fishes = { ...this.state.fishes }
-    // add in our new fish
     const timestamp = Date.now()
+
     fishes[`fish-${ timestamp }`] = fish
-    // set state
-    this.setState({ fishes: fishes })
+
+    this.setState({
+      fishes: fishes
+    })
+  }
+
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    })
   }
 
   render() {
+    const fishList = Object
+                       .keys(this.state.fishes)
+                       .map(key => <Fish key={ key } details={ this.state.fishes[key] } />)
+
     return (
       <div className='catch-of-the-day'>
         <div className='menu'>
           <Header tagline='Fresh Seafood Market'/>
+          <ul className='list-of-fishes'>
+            { fishList }
+          </ul>
         </div>
         <Order />
-        <Inventory addFish={ this.addFish } />
+        <Inventory
+          addFish={ this.addFish }
+          loadSamples={ this.loadSamples }
+        />
       </div>
     )
   }
